666aba41fe73eeeca07f3a801f704c35e5558587
Michael Hillcox <michaelhillcox@gmail.com>
2023-05-11 00:26:05 +0100

    Expanding base mode to support an icon and lang key setup

--- src/main/java/com/direwolf20/buildinggadgets2/util/modes/BaseMode.java
+++ src/main/java/com/direwolf20/buildinggadgets2/util/modes/BaseMode.java
@@ -1,5 +1,6 @@
 package com.direwolf20.buildinggadgets2.util.modes;
 
+import com.direwolf20.buildinggadgets2.common.BuildingGadgets2;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
@@ -14,11 +15,28 @@ public abstract class BaseMode {
      */
     private boolean isExchanging;
 
-    public abstract ResourceLocation getId();
-
     public BaseMode(boolean isExchanging) {
         this.isExchanging = isExchanging;
     }
 
+    /**
+     * Collects a list of blocks that should be used when building and rendering the mode
+     */
     public abstract ArrayList<StatePos> collect(Direction hitSide, Player player, BlockPos start, BlockState state);
+
+    public abstract ResourceLocation getId();
+
+    /**
+     * Used for translations
+     */
+    public String i18n() {
+        return BuildingGadgets2.MODID + ".modes." + this.getId().getPath();
+    }
+
+    /**
+     * Used when displaying the mode selection wheel
+     */
+    public ResourceLocation icon() {
+        return new ResourceLocation(BuildingGadgets2.MODID, "icons/" + getId().getPath());
+    }
 }
