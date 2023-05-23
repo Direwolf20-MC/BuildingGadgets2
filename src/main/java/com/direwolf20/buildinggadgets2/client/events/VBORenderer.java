edf7e45da14ec49c40d806f1d8efa26cd28c63c1
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2023-05-23 16:18:05 -0400

    Really closing in on a good render option, committing before more tweaks

--- src/main/java/com/direwolf20/buildinggadgets2/client/events/VBORenderer.java
+++ src/main/java/com/direwolf20/buildinggadgets2/client/events/VBORenderer.java
@@ -1,5 +1,7 @@
 package com.direwolf20.buildinggadgets2.client.events;
 
+import com.direwolf20.buildinggadgets2.client.renderer.DireBufferBuilder;
+import com.direwolf20.buildinggadgets2.client.renderer.DireVertexConsumer;
 import com.direwolf20.buildinggadgets2.client.renderer.MyRenderMethods;
 import com.direwolf20.buildinggadgets2.util.GadgetNBT;
 import com.direwolf20.buildinggadgets2.util.VectorHelper;
@@ -7,23 +9,27 @@ import com.direwolf20.buildinggadgets2.util.modes.StatePos;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Matrix4f;
+import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.client.model.data.ModelData;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +37,9 @@ import java.util.Optional;
 public class VBORenderer {
     private static VertexBuffer vertexBuffer;
     private static ArrayList<StatePos> statePosCache;
+    private static BufferBuilder.SortState sortState;
+    private static DireBufferBuilder bufferBuilder = new DireBufferBuilder(12582912);
+    private static int sortCounter = 0;
 
     public static void buildRender(RenderLevelStageEvent evt, Player player, ItemStack heldItem) {
         BlockState renderBlockState = GadgetNBT.getGadgetBlockState(heldItem);
@@ -39,24 +48,32 @@ public class VBORenderer {
         var mode = GadgetNBT.getMode(heldItem);
         BlockHitResult lookingAt = VectorHelper.getLookingAt(player, heldItem);
         ArrayList<StatePos> buildList = mode.collect(lookingAt.getDirection(), player, lookingAt.getBlockPos(), renderBlockState);
-        /*for (int i = 1; i < 4; i++) {
-            buildList.add(new StatePos(Blocks.COBBLESTONE.defaultBlockState(), new BlockPos(1, 0, i)));
-            buildList.add(new StatePos(Blocks.OAK_LOG.defaultBlockState(), new BlockPos(2, 0, i)));
-        }*/
+        for (int k = -15; k < 15; k++) {
+            for (int j = 1; j < 25; j++) {
+                for (int i = -1; i < 25; i++) {
+                    buildList.add(new StatePos(Blocks.GLASS.defaultBlockState(), new BlockPos(k, i, -j)));
+                    //buildList.add(new StatePos(Blocks.OAK_LOG.defaultBlockState(), new BlockPos(2, 0, i)));
+                    buildList.add(new StatePos(Blocks.COBBLESTONE.defaultBlockState(), new BlockPos(k, i, j)));
+                }
+            }
+        }
         if (buildList.equals(statePosCache))
             return;
 
         System.out.println("I'm Building!");
+        Long drawStart = System.nanoTime();
         Level level = player.level;
         //player.displayClientMessage(Component.literal("Rebuilding Render due to change." + level.getGameTime()), false);
         statePosCache = buildList;
         Tesselator tesselator = Tesselator.getInstance();
-        BufferBuilder bufferbuilder = tesselator.getBuilder();
+        //bufferBuilder = new DireBufferBuilder(512);
         vertexBuffer = new VertexBuffer();
-        bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.BLOCK);
+        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.BLOCK);
         PoseStack matrix = new PoseStack(); //Create a new matrix stack for use in the buffer building process
         BlockRenderDispatcher dispatcher = Minecraft.getInstance().getBlockRenderer();
-
+        DireVertexConsumer direVertexConsumer = new DireVertexConsumer(bufferBuilder, 0.5f);
+        RenderType renderType = RenderType.translucent();
+        //renderType.setupRenderState();
         for (StatePos pos : buildList.stream().filter(pos -> pos.isModelRender).toList()) {
 
             //matrix.translate(-0.005f, -0.005f, -0.005f);
@@ -69,19 +86,33 @@ public class VBORenderer {
             float f2 = (float) (color & 255) / 255.0F;
             matrix.pushPose();
             matrix.translate(pos.pos.getX(), pos.pos.getY(), pos.pos.getZ());
-            for (Direction direction : Direction.values()) {
-                Optional<StatePos> statePosOptional = buildList.stream().filter(obj -> obj.pos.equals(pos.pos.relative(direction))).findFirst();
-                if (statePosOptional.isEmpty() || !(statePosOptional.get().state.isSolidRender(level, statePosOptional.get().pos)))
-                    MyRenderMethods.renderModelBrightnessColorQuads(matrix.last(), bufferbuilder, f, f1, f2, 0.5f, ibakedmodel.getQuads(pos.state, direction, RandomSource.create(), ModelData.EMPTY, RenderType.cutout()), 15728640, 655360);
+            dispatcher.renderBatched(pos.state, pos.pos.offset(lookingAt.getBlockPos()), level, matrix, direVertexConsumer, true, RandomSource.create(), ModelData.EMPTY, RenderType.solid());
+            /*for (Direction direction : Direction.values()) {
+                //if (shouldRenderSide(buildList, pos, direction, level))
+                    MyRenderMethods.renderModelBrightnessColorQuads(matrix.last(), bufferbuilder, f, f1, f2, 0.5f, ibakedmodel.getQuads(pos.state, direction, RandomSource.create(), ModelData.EMPTY, RenderType.translucent()), 15728640, 655360);
             }
-            MyRenderMethods.renderModelBrightnessColorQuads(matrix.last(), bufferbuilder, f, f1, f2, 0.5f, ibakedmodel.getQuads(pos.state, null, RandomSource.create(), ModelData.EMPTY, RenderType.cutout()), 15728640, 655360);
+            MyRenderMethods.renderModelBrightnessColorQuads(matrix.last(), bufferbuilder, f, f1, f2, 0.5f, ibakedmodel.getQuads(pos.state, null, RandomSource.create(), ModelData.EMPTY, RenderType.translucent()), 15728640, 655360);
+            */
             matrix.popPose();
 
         }
-
+        //renderType.clearRenderState();
+        Vec3 projectedView = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+        Vector3f sortPos = new Vector3f(projectedView.subtract(lookingAt.getBlockPos().getX(), lookingAt.getBlockPos().getY(), lookingAt.getBlockPos().getZ()));
+        bufferBuilder.setQuadSortOrigin(sortPos.x(), sortPos.y(), sortPos.z());
+        sortState = bufferBuilder.getSortState();
         vertexBuffer.bind();
-        vertexBuffer.upload(bufferbuilder.end());
+        vertexBuffer.upload(bufferBuilder.end());
         VertexBuffer.unbind();
+        //bufferBuilder.clear();
+        Long drawEnd = System.nanoTime();
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        System.out.println("Draw time: " + numberFormat.format(drawEnd - drawStart));
+    }
+
+    public static boolean shouldRenderSide(ArrayList<StatePos> buildList, StatePos pos, Direction direction, Level level) {
+        Optional<StatePos> statePosOptional = buildList.stream().filter(obj -> obj.pos.equals(pos.pos.relative(direction))).findFirst();
+        return (statePosOptional.isEmpty() || !(statePosOptional.get().state.isSolidRender(level, statePosOptional.get().pos)));
     }
 
     public static void drawRender(RenderLevelStageEvent evt, Player player, ItemStack heldItem) {
@@ -101,9 +132,37 @@ public class VBORenderer {
         matrix.pushPose();
         matrix.translate(-projectedView.x(), -projectedView.y(), -projectedView.z());
         matrix.translate(lookingAt.getBlockPos().getX(), lookingAt.getBlockPos().getY(), lookingAt.getBlockPos().getZ());
-        vertexBuffer.bind();
-        vertexBuffer.drawWithShader(matrix.last().pose(), new Matrix4f(evt.getProjectionMatrix()), RenderSystem.getShader());
-        VertexBuffer.unbind();
+        //Vector3f sortPos = new Vector3f((float)projectedView.x - lookingAt.getBlockPos().getX(), (float)projectedView.y - lookingAt.getBlockPos().getY(), (float)projectedView.z - lookingAt.getBlockPos().getZ());
+        //Vector3f sortPos = new Vector3f(projectedView.subtract(lookingAt.getBlockPos().getX(), lookingAt.getBlockPos().getY(), lookingAt.getBlockPos().getZ()));
+        NumberFormat numberFormat = NumberFormat.getInstance();
+
+        try {
+            vertexBuffer.bind();
+            if (sortCounter > 120) {
+                long sortStart = System.nanoTime();
+                BufferBuilder.RenderedBuffer sortedBuffer = sort(lookingAt.getBlockPos());
+                long sortEnd = System.nanoTime();
+                //System.out.println("Sorting took: " + numberFormat.format(sortEnd - sortStart));
+                vertexBuffer.upload(sortedBuffer);
+                sortCounter = 0;
+            } else {
+                sortCounter++;
+            }
+            long renderStart = System.nanoTime();
+            /*BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
+            bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.BLOCK);
+            bufferBuilder.restoreSortState(sortState);
+            bufferBuilder.setQuadSortOrigin(-sortPos.x(), -sortPos.y(), -sortPos.z());*/
+            vertexBuffer.drawWithShader(matrix.last().pose(), new Matrix4f(evt.getProjectionMatrix()), RenderSystem.getShader());
+            VertexBuffer.unbind();
+            //bufferBuilder.clear();
+            long renderEnd = System.nanoTime();
+            long totalTime = renderEnd - renderStart;
+            //if (totalTime > 25000)
+            //System.out.println("Rendering took: " + numberFormat.format(totalTime));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         matrix.popPose();
 
         MultiBufferSource.BufferSource buffersource = Minecraft.getInstance().renderBuffers().bufferSource();
@@ -132,5 +191,16 @@ public class VBORenderer {
             //MyRenderMethods.renderBETransparent(renderBlockState, matrix, buffersource, 15728640, 655360, 0.5f);
             matrix.popPose();
         }*/
+    }
+
+    public static BufferBuilder.RenderedBuffer sort(BlockPos lookingAt) {
+        BufferBuilder bufferBuilder2 = Tesselator.getInstance().getBuilder();
+        Vec3 projectedView = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+        Vector3f sortPos = new Vector3f(projectedView.subtract(lookingAt.getX(), lookingAt.getY(), lookingAt.getZ()));
+        //bufferBuilder = new DireBufferBuilder(12582912);
+        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.BLOCK);
+        bufferBuilder.restoreSortState(sortState);
+        bufferBuilder.setQuadSortOrigin(sortPos.x(), sortPos.y(), sortPos.z());
+        return bufferBuilder.end();
     }
 }
---
+++ src/main/java/com/direwolf20/buildinggadgets2/client/renderer/DireBufferBuilder.java
@@ -0,0 +1,45 @@
+package com.direwolf20.buildinggadgets2.client.renderer;
+
+import com.google.common.primitives.Floats;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import it.unimi.dsi.fastutil.ints.IntArrays;
+import it.unimi.dsi.fastutil.ints.IntConsumer;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class DireBufferBuilder extends BufferBuilder {
+    public DireBufferBuilder(int pCapacity) {
+        super(pCapacity);
+    }
+
+    @Override
+    public void putSortedQuadIndices(VertexFormat.IndexType pIndexType) {
+        float[] afloat = new float[this.sortingPoints.length];
+        int[] aint = new int[this.sortingPoints.length];
+
+        for (int i = 0; i < this.sortingPoints.length; aint[i] = i++) {
+            float f = this.sortingPoints[i].x() - this.sortX;
+            float f1 = this.sortingPoints[i].y() - this.sortY;
+            float f2 = this.sortingPoints[i].z() - this.sortZ;
+            afloat[i] = f * f + f1 * f1 + f2 * f2;
+        }
+
+        IntArrays.mergeSort(aint, (p_166784_, p_166785_) -> {
+            return Floats.compare(afloat[p_166785_], afloat[p_166784_]);
+        });
+        IntConsumer intconsumer = this.intConsumer(this.nextElementByte, pIndexType);
+
+        // Reverse the order of the sorted indices.
+        ArrayUtils.reverse(aint);
+
+        for (int j : aint) {
+            intconsumer.accept(j * this.mode.primitiveStride + 0);
+            intconsumer.accept(j * this.mode.primitiveStride + 1);
+            intconsumer.accept(j * this.mode.primitiveStride + 2);
+            intconsumer.accept(j * this.mode.primitiveStride + 2);
+            intconsumer.accept(j * this.mode.primitiveStride + 3);
+            intconsumer.accept(j * this.mode.primitiveStride + 0);
+        }
+
+    }
+}
