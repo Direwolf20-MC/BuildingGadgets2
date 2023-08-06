2900d87a9cce0c6efdf892f7ec9153a778740e02
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2023-08-06 12:20:52 -0400

    PlaceOnTop for BuildToMe goes to players eye rather than feet

--- src/main/java/com/direwolf20/buildinggadgets2/util/modes/BuildToMe.java
+++ src/main/java/com/direwolf20/buildinggadgets2/util/modes/BuildToMe.java
@@ -1,11 +1,14 @@
 package com.direwolf20.buildinggadgets2.util.modes;
 
 import com.direwolf20.buildinggadgets2.BuildingGadgets2;
+import com.direwolf20.buildinggadgets2.common.items.BaseGadget;
+import com.direwolf20.buildinggadgets2.util.GadgetNBT;
 import com.direwolf20.buildinggadgets2.util.datatypes.StatePos;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.ArrayList;
@@ -23,9 +26,12 @@ public class BuildToMe extends BaseMode {
     @Override
     public ArrayList<StatePos> collectWorld(Direction hitSide, Player player, BlockPos start, BlockState state) {
         ArrayList<StatePos> coordinates = new ArrayList<>();
+        ItemStack gadget = BaseGadget.getGadget(player);
+        boolean placeontop = GadgetNBT.getSetting(gadget, "placeontop");
 
         int startCoord = hitSide.getAxis().choose(start.getX(), start.getY(), start.getZ());
-        int playerCoord = hitSide.getAxis().choose(player.blockPosition().getX(), player.blockPosition().getY(), player.blockPosition().getZ());
+        double yPos = placeontop ? player.getEyeY() : player.getBlockY();
+        int playerCoord = hitSide.getAxis().choose(player.blockPosition().getX(), (int) yPos, player.blockPosition().getZ());
 
         // Clamp the value to the max range of the gadgets raytrace
         double difference = Math.max(0, Math.min(32, Math.abs(startCoord - playerCoord)));
