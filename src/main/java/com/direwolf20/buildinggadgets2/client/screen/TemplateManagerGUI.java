b0c48d475deeedc4f09deccd6042081c148b99cb
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2023-08-20 11:22:29 -0400

    Dragging works!

--- src/main/java/com/direwolf20/buildinggadgets2/client/screen/TemplateManagerGUI.java
+++ src/main/java/com/direwolf20/buildinggadgets2/client/screen/TemplateManagerGUI.java
@@ -167,9 +167,9 @@ public class TemplateManagerGUI extends AbstractContainerScreen<TemplateManagerC
         PoseStack poseStack = RenderSystem.getModelViewStack();
         poseStack.pushPose();
         poseStack.setIdentity();
-        poseStack.translate(-lengthZ / 2, -lengthY / 2, -lengthZ); //Move the objects in the world being drawn inside the viewport around
-        poseStack.mulPose(new Quaternionf().setAngleAxis(40f / 180 * (float) Math.PI, 1, 0, 0)); //Rotate
-        poseStack.mulPose(new Quaternionf().setAngleAxis(90f / 180 * (float) Math.PI, 0, 1, 0)); //Rotate
+        poseStack.translate(-lengthZ / 2 + panX, -lengthY / 2 + panY, -lengthZ + zoom); //Move the objects in the world being drawn inside the viewport around
+        poseStack.mulPose(new Quaternionf().setAngleAxis(rotX / 180 * (float) Math.PI, 1, 0, 0)); //Rotate
+        poseStack.mulPose(new Quaternionf().setAngleAxis(rotY / 180 * (float) Math.PI, 0, 1, 0)); //Rotate
         RenderSystem.applyModelViewMatrix();
         RenderSystem.clear(GL11.GL_DEPTH_BUFFER_BIT, false); //Clear the depth buffer so it can draw where it is
 
@@ -208,215 +208,6 @@ public class TemplateManagerGUI extends AbstractContainerScreen<TemplateManagerC
         }
 
         this.nameField.render(guiGraphics, mouseX, mouseY, partialTicks);
-    }
-
-    /*private void validateCache(float partialTicks) {
-        // Invalidate the render
-        if (container.getSlot(0).getItem().isEmpty() && template != null) {
-            template = null;
-            resetViewport();
-            return;
-        }
-
-        container.getSlot(0).getItem().getCapability(CapabilityTemplate.TEMPLATE_KEY_CAPABILITY).ifPresent(key -> templateProvider.ifPresent(provider -> {
-            // Make sure we're not re-creating the same cache.
-            Template template = provider.getTemplateForKey(key);
-            if (this.template == template)
-                return;
-
-            this.template = template;
-
-//            IBuildView view = template.createViewInContext(
-//                    SimpleBuildContext.builder()
-//                            .player(getMinecraft().player)
-//                            .stack(container.getSlot(0).getStack())
-//                            .build(new MockDelegationWorld(getMinecraft().world)));
-
-//            int displayList = GLAllocation.generateDisplayLists(1);
-//            GlStateManager.newList(displayList, GL11.GL_COMPILE);
-
-//            renderStructure(view, partialTicks);
-
-//            GlStateManager.endList();
-//            this.displayList = displayList;
-        }));
-    }
-
-    private void renderStructure(IBuildView view, float partialTicks) {
-        Random rand = new Random();
-        BlockRenderDispatcher dispatcher = getMinecraft().getBlockRenderer();
-
-        BufferBuilder bufferBuilder = new BufferBuilder(2097152);
-        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.BLOCK);
-
-        for (PlacementTarget target : view) {
-            target.placeIn(view.getContext());
-            BlockPos targetPos = target.getPos();
-            BlockState renderBlockState = view.getContext().getWorld().getBlockState(targetPos);
-            BlockEntity te = view.getContext().getWorld().getBlockEntity(targetPos);
-
-            if (renderBlockState.getRenderShape() == RenderShape.MODEL) {
-                BakedModel model = dispatcher.getBlockModel(renderBlockState);
-//                dispatcher.getBlockModelRenderer().renderModelFlat()
-//                        .renderModelFlat(getWorld(), model, renderBlockState, target.getPos(), bufferBuilder, false,
-//                        rand, 0L, te != null ? te.getModelData() : EmptyModelData.INSTANCE);
-            }
-
-            if (te != null) {
-                try {
-                    BlockEntityRenderer<BlockEntity> renderer = Minecraft.getInstance().getBlockEntityRenderDispatcher().getRenderer(te);
-                    if (renderer != null) {
-//                        if (te.hasFastRenderer())
-//                            renderer.renderTileEntityFast(te, targetPos.getX(), targetPos.getY(), targetPos.getZ(), partialTicks, - 1, bufferBuilder);
-//                        else
-//                            renderer.render(te, targetPos.getX(), targetPos.getY(), targetPos.getZ(), partialTicks, - 1);
-                    }
-                    //remember vanilla Tiles rebinding the TextureAtlas
-                    RenderSystem.setShaderTexture(0, InventoryMenu.BLOCK_ATLAS);
-                } catch (Exception e) {
-                    BuildingGadgets.LOG.error("Error rendering TileEntity", e);
-                }
-            }
-        }
-
-        bufferBuilder.end();
-
-//        if (bufferBuilder.getVertexCount() > 0) {
-//            VertexFormat vertexformat = bufferBuilder.getVertexFormat();
-//            int i = vertexformat.getSize();
-//            ByteBuffer bytebuffer = bufferBuilder.getByteBuffer();
-//            List<VertexFormatElement> list = vertexformat.getElements();
-//
-//            for (int j = 0; j < list.size(); ++ j) {
-//                VertexFormatElement vertexformatelement = list.get(j);
-//                bytebuffer.position(vertexformat.getOffset(j));
-//                vertexformatelement.getUsage().preDraw(vertexformat, j, i, bytebuffer);
-//            }
-//
-//            GlStateManager.drawArrays(bufferBuilder.getDrawMode(), 0, bufferBuilder.getVertexCount());
-//            int i1 = 0;
-//
-//            for (int j1 = list.size(); i1 < j1; ++ i1) {
-//                VertexFormatElement vertexformatelement1 = list.get(i1);
-//                vertexformatelement1.getUsage().postDraw(vertexformat, i1, i, bytebuffer);
-//            }
-//        }
-    }
-
-    private void renderRequirement(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-        MaterialList requirements = this.template.getHeaderAndForceMaterials(BuildContext.builder().build(getWorld())).getRequiredItems();
-        if (requirements == null)
-            return;
-
-        Lighting.setupForFlatItems();
-
-        guiGraphics.pose().pushPose();
-        guiGraphics.pose().translate(leftPos - 30, topPos - 5, 200);
-        guiGraphics.pose().scale(.8f, .8f, .8f);
-
-        String title = "Requirements"; // Todo lang;
-        guiGraphics.drawString(getMinecraft().font, title, 5 - (font.width(title)), 0, Color.WHITE.getRGB());
-
-        // The things you have to do to get anything from this system is just stupid.
-        MatchResult list = InventoryHelper.CREATIVE_INDEX.tryMatch(requirements);
-        ImmutableMultiset<IUniqueObject<?>> foundItems = list.getFoundItems();
-
-        // Reverse sorted list of items required.
-        List<Multiset.Entry<IUniqueObject<?>>> sortedEntries = ImmutableList.sortedCopyOf(Comparator
-                .<Multiset.Entry<IUniqueObject<?>>, Integer>comparing(Multiset.Entry::getCount)
-                .reversed(), list.getChosenOption().entrySet());
-
-        int index = 0, column = 0;
-        for (Multiset.Entry<IUniqueObject<?>> e : sortedEntries) {
-            ItemStack stack = e.getElement().createStack();
-            int x = (-20 - (column * 25)), y = (20 + (index * 25));
-
-            guiGraphics.renderItem(stack, x + 4, y + 4);
-            guiGraphics.renderItemDecorations(Minecraft.getInstance().font, stack, x + 4, y + 4, GadgetUtils.withSuffix(foundItems.count(e.getElement())));
-
-            int space = (int) (25 - (.2f * 25));
-            int zoneX = ((leftPos - 32) + (-15 - (column * space))), zoneY = (topPos - 9) + (20 + (index * space));
-
-            if (mouseX > zoneX && mouseX < (zoneX + space) && mouseY > zoneY && mouseY < (zoneY + space)) {
-                guiGraphics.renderTooltip(font, Lists.transform(stack.getTooltipLines(this.getMinecraft().player, TooltipFlag.Default.NORMAL), Component::getVisualOrderText), x + 15, y + 25);
-            }
-
-            index++;
-            if (index % 8 == 0) {
-                column++;
-                index = 0;
-            }
-        }
-
-        Lighting.setupFor3DItems();
-        guiGraphics.pose().popPose();
-    }
-
-    private void renderPanel() {
-//        double scale = getMinecraft().getWindow().getGuiScale();
-//
-//        BlockPos startPos = template.getHeader().getBoundingBox().getMin();
-//        BlockPos endPos = template.getHeader().getBoundingBox().getMax();
-//
-//        double lengthX = Math.abs(startPos.getX() - endPos.getX());
-//        double lengthY = Math.abs(startPos.getY() - endPos.getY());
-//        double lengthZ = Math.abs(startPos.getZ() - endPos.getZ());
-//
-//        final double maxW = 6 * 16;
-//        final double maxH = 11 * 16;
-//
-//        double overW = Math.max(lengthX * 16 - maxW, lengthZ * 16 - maxW);
-//        double overH = lengthY * 16 - maxH;
-//
-//        double sc = 1;
-//        double zoomScale = 1;
-//
-//        if (overW > 0 && overW >= overH) {
-//            sc = maxW / (overW + maxW);
-//            zoomScale = overW / 40;
-//        } else if (overH > 0 && overH >= overW) {
-//            sc = maxH / (overH + maxH);
-//            zoomScale = overH / 40;
-//        }
-//
-//        RenderSystem.pushMatrix();
-//        RenderSystem.matrixMode(GL11.GL_PROJECTION);
-//        RenderSystem.pushMatrix();
-//        RenderSystem.loadIdentity();
-//
-//        RenderSystem.multMatrix(Matrix4f.perspective(60, (float) panel.getWidth() / panel.getHeight(), 0.01F, 4000));
-//        RenderSystem.matrixMode(GL11.GL_MODELVIEW);
-//        RenderSystem.viewport((int) Math.round((leftPos + panel.getX()) * scale),
-//                (int) Math.round(getMinecraft().getWindow().getHeight() - (topPos + panel.getY() + panel.getHeight()) * scale),
-//                (int) Math.round(panel.getWidth() * scale),
-//                (int) Math.round(panel.getHeight() * scale));
-//
-//        RenderSystem.clear(GL11.GL_DEPTH_BUFFER_BIT, true);
-//
-//        sc = (293 * sc) + zoom / zoomScale;
-//        RenderSystem.scaled(sc, sc, sc);
-//        int moveX = startPos.getX() - endPos.getX();
-//
-//        RenderSystem.rotatef(30, 0, 1, 0);
-//        if (startPos.getX() >= endPos.getX())
-//            moveX--;
-//
-//        RenderSystem.translated((moveX) / 1.75, -Math.abs(startPos.getY() - endPos.getY()) / 1.75, 0);
-//        RenderSystem.translated(panX, -panY, 0);
-//        RenderSystem.translated(((startPos.getX() - endPos.getX()) / 2f) * -1, ((startPos.getY() - endPos.getY()) / 2f) * -1, ((startPos.getZ() - endPos.getZ()) / 2f) * -1);
-//        RenderSystem.rotatef(-rotX, 1, 0, 0);
-//        RenderSystem.rotatef(rotY, 0, 1, 0);
-//        RenderSystem.translated(((startPos.getX() - endPos.getX()) / 2f), ((startPos.getY() - endPos.getY()) / 2f), ((startPos.getZ() - endPos.getZ()) / 2f));
-//
-//        getMinecraft().getTextureManager().bind(InventoryMenu.BLOCK_ATLAS);
-//
-////        RenderSystem.callList(displayList);
-//
-//        RenderSystem.popMatrix();
-//        RenderSystem.matrixMode(GL11.GL_PROJECTION);
-//        RenderSystem.popMatrix();
-//        RenderSystem.matrixMode(GL11.GL_MODELVIEW);
-//        RenderSystem.viewport(0, 0, getMinecraft().getWindow().getWidth(), getMinecraft().getWindow().getHeight());
     }
 
     private void resetViewport() {
@@ -461,7 +252,7 @@ public class TemplateManagerGUI extends AbstractContainerScreen<TemplateManagerC
         }
 
         return this.nameField.isFocused() ? this.nameField.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_) : super.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_);
-    }*/
+    }
 
     @Override
     protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
@@ -471,11 +262,13 @@ public class TemplateManagerGUI extends AbstractContainerScreen<TemplateManagerC
                 float prevRotY = rotY;
                 rotX = initRotX - ((int) getMinecraft().mouseHandler.ypos() - clickY);
                 rotY = initRotY + ((int) getMinecraft().mouseHandler.xpos() - clickX);
-                momentumX = rotX - prevRotX;
-                momentumY = rotY - prevRotY;
+                //momentumX = rotX - prevRotX;
+                //momentumY = rotY - prevRotY;
             } else if (clickButton == 1) {
                 panX = initPanX + ((int) getMinecraft().mouseHandler.xpos() - clickX) / 8f;
                 panY = initPanY + ((int) getMinecraft().mouseHandler.ypos() - clickY) / 8f;
+            } else if (clickButton == 2) {
+                resetViewport();
             }
         }
 
@@ -491,6 +284,7 @@ public class TemplateManagerGUI extends AbstractContainerScreen<TemplateManagerC
         if (buttonSave.isHovered() || buttonLoad.isHovered() || buttonPaste.isHovered())
             drawSlotOverlay(guiGraphics, buttonLoad.isHovered() ? container.getSlot(0) : container.getSlot(1));
 
+
     }
 
     private void drawSlotOverlay(GuiGraphics guiGraphics, Slot slot) {
@@ -502,7 +296,7 @@ public class TemplateManagerGUI extends AbstractContainerScreen<TemplateManagerC
 
     @Override
     public boolean mouseScrolled(double mouseX, double mouseY, double scrollDelta) {
-        zoom = initZoom + ((float) scrollDelta * 20);
+        zoom = initZoom + ((float) scrollDelta * 2);
         if (zoom < -200) zoom = -200;
         if (zoom > 5000) zoom = 5000;
 
@@ -522,19 +316,6 @@ public class TemplateManagerGUI extends AbstractContainerScreen<TemplateManagerC
             initPanY = panY;
         }
     }
-
-//    @Override
-//    public void tick() {
-//        super.tick();
-//        nameField.tick();
-//        if (! panelClicked) {
-//            initRotX = rotX;
-//            initRotY = rotY;
-//            initZoom = zoom;
-//            initPanX = panX;
-//            initPanY = panY;
-//        }
-//    }
 
     private void rename(ItemStack stack) {
         if (nameField.getValue().isEmpty())
